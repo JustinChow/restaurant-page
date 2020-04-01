545f486d5c78247e5e90586626719e435bd19037
Justin Chow <justin.chow@queensu.ca>
2020-03-31 20:51:56 -0400

    Updates

---
+++ precache-manifest.e5fa189d26277708a7dcbed9310bd77c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8b892cbf6639f6ccc7742e4b273b4a",
+    "url": "/restaurant-page/index.html"
+  },
+  {
+    "revision": "b2b2884d368ade81ae0c",
+    "url": "/restaurant-page/static/css/main.e78c70a3.chunk.css"
+  },
+  {
+    "revision": "1c1cddac023ef4133119",
+    "url": "/restaurant-page/static/js/2.fd0f5113.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-page/static/js/2.fd0f5113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b2884d368ade81ae0c",
+    "url": "/restaurant-page/static/js/main.afc897bc.chunk.js"
+  },
+  {
+    "revision": "fc6caef2cb3a1be45ebe",
+    "url": "/restaurant-page/static/js/runtime-main.999c407d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e26b5daf289a62ca1885f4efc129b693",
+    "url": "/restaurant-page/static/media/restaurant-image.e26b5daf.jpg"
+  }
+]);
